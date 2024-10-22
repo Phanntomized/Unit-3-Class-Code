ef20ed16b163e04eb68c249438a0b817f629bac2
Atticus P. Boon <boona@wlhs.wlwv.k12.or.us>
2024-10-22 17:02:48 +0000

    debug

--- seasons.py
+++ seasons.py
@@ -3,6 +3,7 @@ Name: Phann Boon
 Date: 10/22/24
 Assignment: Unit 2 and 3 Project
 '''
+
 def start():
     try:
         month = input("Enter the name of the month: ")
@@ -24,19 +25,9 @@ def start():
             season = "Winter"
         elif month == "december" and 1 <= day < 21:
             season = "Fall"
-        #else:
-            #if not month == "january" or "february" or "march" or "april" or "may" or "june" or "july" or "august" or "september" or "october" or "november" or "december":
-                #print("Invalid month.")
-            #elif not 1 <= day <= 31:
-                #print("Invalid day.")
-            #else:
-                #print(f"{month.title()} {day} is in {season}.")
+        print(f"{month.title()} {day} is in {season}.")
     except:
         print("Invalid response.")
         start()
 
 start()
-
-if month == "january" or "february" or "march" or "april" or "may" or "june" or "july" or "august" or "september" or "october" or "november" or "december":
-    if 1 <= day <= 31:
-        print(f"{month.title()} {day} is in {season}.")
